945bed07897d8009153facb32548fc8d77f247f1
Andre Medeiros <andre.medeiros@futurice.com>
2014-08-27 13:46:36 +0300

    Initial commit

---
+++ android/src/com/futurice/sankogame/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.futurice.sankogame.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.futurice.sankogame.SankoGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new SankoGame(), config);
+	}
+}
